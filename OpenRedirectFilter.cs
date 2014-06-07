b6841fd256c35817d73b31a70003bda7b51ac6cd
egomadness <egomaddness@yahoo.com>
2014-06-06 19:07:02 -0700

    MVC Action Filter that prevents open redirects

---
+++ OpenRedirectFilter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CredoMobile.WebUI.Filters
+{
+    /// <summary>
+    /// This filter prevents the site from redirecting to external hosts
+    /// </summary>
+    public class OpenRedirectFilter : ActionFilterAttribute
+    {
+
+        private const string homePageRoute = "HomePage";
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+
+            var result = filterContext.Result as RedirectResult;
+            if (result != null)
+            {
+                var request = filterContext.HttpContext.Request;
+                if (!UrlHostEqualsServerHost(result.Url, request))
+                {
+                   
+                    filterContext.Result = new RedirectToRouteResult(homePageRoute, null);
+
+                }
+
+
+            }
+
+
+
+        }
+
+
+        private bool UrlHostEqualsServerHost(string url, HttpRequestBase requestBase)
+        {
+            if (!url.Contains("://"))
+                url = "http://" + url;
+
+            return (requestBase.Url != null && (new Uri(url)).Host == requestBase.Url.Host);
+        }
+
+    }
+}
